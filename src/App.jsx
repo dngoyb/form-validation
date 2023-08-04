14037178b88467547d935efd567403621da77c8f
Ngoy banza <dngoyb@gmail.com>
2023-08-04 19:11:01 +0200

    Validation complete, The End

--- src/App.jsx
+++ src/App.jsx
@@ -18,43 +18,55 @@ function App() {
 			name: 'username',
 			type: 'text',
 			placeholder: 'Username',
+			errorMessage:
+				"Username should be 3-16 characters and shouldn't include any special character!",
 			label: 'Username',
+			pattern: '^[A-Za-z0-9]{3,16}$',
+			required: true,
 		},
 		{
 			id: 2,
 			name: 'email',
 			type: 'email',
 			placeholder: 'Email',
+			errorMessage: 'It should be a valid email address!',
 			label: 'Email',
+			required: true,
 		},
 		{
 			id: 3,
 			name: 'birtday',
-			type: 'text',
+			type: 'date',
 			placeholder: 'Birtday',
+			errorMessage: '',
 			label: 'Birtday',
 		},
 		{
 			id: 4,
 			name: 'password',
-			type: 'text',
+			type: 'password',
 			placeholder: 'Password',
+			errorMessage:
+				'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
 			label: 'Password',
+			pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+			required: true,
 		},
 		{
 			id: 5,
 			name: 'confirmPassword',
-			type: 'text',
+			type: 'password',
 			placeholder: 'Confirm Password',
+			errorMessage: "Passwords don't match!",
 			label: 'Confirm Password',
+			pattern: values.password,
+			required: true,
 		},
 	];
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 	};
-
-	console.log(values);
 
 	const onChange = (e) => {
 		setValues({ ...values, [e.target.name]: e.target.value });
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './formInput.css';
-const FormInput = ({ label, onChange, id, ...inputProps }) => {
+const FormInput = ({ label, onChange, id, errorMessage, ...inputProps }) => {
+	const [focused, setFocused] = useState(false);
 	return (
 		<div className='form-input'>
 			<label htmlFor=''>{label}</label>
-			<input {...inputProps} onChange={onChange} />
+			<input
+				{...inputProps}
+				onChange={onChange}
+				onBlur={() => setFocused(true)}
+				onFocus={() =>
+					inputProps.name === 'confirmedPassword' && setFocused(true)
+				}
+				focused={focused.toString()}
+			/>
+			<span>{errorMessage}</span>
 		</div>
 	);
 };
